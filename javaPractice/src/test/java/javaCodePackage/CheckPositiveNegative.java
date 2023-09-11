0dce71eb0030b84ce7edf46c4fcadd70dfe132d5
Suyog Waghmare <suyog.waghmare@encora.com>
2023-09-11 13:03:51 +0530

    add positive and negative code

---
+++ javaPractice/src/test/java/javaCodePackage/CheckPositiveNegative.java
@@ -0,0 +1,22 @@
+package javaCodePackage;
+
+public class CheckPositiveNegative {
+    public static void main(String[] args) {
+
+        int input;
+        input=30;
+        if(input>0){
+            System.out.println("Number is positive");
+        }
+        else if (input<0){
+            System.out.println("Number is negative");
+        }
+        else {
+            System.out.println("Number is zero");
+        }
+    }
+}
+//Write a Java program to get a number from the user and print whether it is positive or negative.
+//Test Data
+//Input number: 35
+//OutPut:Number is positive
