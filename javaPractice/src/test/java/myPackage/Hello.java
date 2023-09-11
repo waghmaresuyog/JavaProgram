0a2c0fe208409b974a0526eb706576e4b22dd590
Suyog Waghmare <suyog.waghmare@encora.com>
2023-09-11 11:45:49 +0530

    update first code

--- javaPractice/src/test/java/myPackage/Hello.java
+++ javaPractice/src/test/java/myPackage/Hello.java
@@ -2,6 +2,9 @@ package myPackage;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        System.out.print("Hello");
+        System.out.println("  suyog");
     }
+
 }
